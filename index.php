0dd2596583989275c8fa04856b4dde01b347d91c
Carlos Eduardo <silvacarlos02@sicredi.com.br>
2023-11-24 16:54:51 -0300

    fix: removed firebase config

--- index.php
+++ index.php
@@ -1,32 +1,5 @@
 <?php
 
-echo "<script language='javascript' type='module'>
-    import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js';
-    console.log('script firebase iniciado...')
-
-    const firebaseConfig = {
-        apiKey: 'AIzaSyAjo-2T-nR7hJ1NvNtbGdP3c62Tn3lYvo4',
-        authDomain: 'social-compass-server.firebaseapp.com',
-        projectId: 'social-compass-server',
-        storageBucket: 'social-compass-server.appspot.com',
-        messagingSenderId: '953915826162',
-        appId: '1:953915826162:web:7cc721680df45f11ac556b'
-    };
-
-    const app = initializeApp(firebaseConfig);
-</script>";
-
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| First we need to get an application instance. This creates an instance
-| of the application / container and bootstraps the application so it
-| is ready to receive HTTP / Console requests from the environment.
-|
-*/
-
 $app = require __DIR__.'/bootstrap/app.php';
 
 /*
